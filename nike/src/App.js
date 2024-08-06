12eec072735afea924019fb2a9b324d7400739a3
VishalGCnetric <vishal@cnetric.com>
2024-08-06 12:09:33 +0530

    Initial_commit

---
+++ nike/src/App.js
@@ -0,0 +1,12 @@
+// src/App.js
+function App() {
+  return (
+    <div className="text-center">
+      <header className="bg-blue-500 text-white p-6">
+        <h1 className="text-4xl font-bold">Hello Tailwind!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
