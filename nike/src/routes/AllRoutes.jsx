3c774073ff1406463d253c3045cfdca52ad22c82
VishalGCnetric <vishal@cnetric.com>
2024-08-08 10:39:25 +0530

    checkout

--- nike/src/routes/AllRoutes.jsx
+++ nike/src/routes/AllRoutes.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Homepage from '../Pages/Homepage'
 import CartPage from '../Component/cart/CartPage'
+import SignUp from '../Pages/SignUp'
+import SingIn from '../Pages/SingIn'
+import Order from '../Pages/Order'
 
 const AllRoutes = () => {
   return (
     <Routes>
+             <Route path='/auth/signup' element={<SignUp/>}/>
+             <Route path='/auth/signin' element={<SingIn/>}/>
+
        <Route path='/' element={<Homepage/>}/>
        <Route path='/cart' element={<CartPage/>}/>
+       <Route path= "/checkout/orderSummary" element={<Order/>}/>
     </Routes>
   )
 }
